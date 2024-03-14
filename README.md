c333451b12fa6af9a23eef5fd03595f9e4f1f4ae
Wernfried <github@domscheit.ch>
2024-03-14 15:14:00 +0100

    Rename compressed files

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # minify-all-js
 
-A function that minifies your javascript files (recursively).
-**minify-all-js** was designed to reduce the size of your project's `node_modules` directory. But it can also be used to minify any js files. By giving it a directory, `minify-all-js` will walk through the depth of your folders and minify all the javascript that it sees.
+A function that minifies your javascript files (recursively when `--overwrite`).
+**minify-all-js** was designed to reduce the size of your project's `node_modules` directory. But it can also be used to minify any js files. By giving it a directory and option `--overwrite`, `minify-all-js` will walk through the depth of your folders and minify all the javascript that it sees.
+
+Without `--overwrite` (the default), `minify-all-js` does not scan directories recursively and files are renamed to "<old name>.min.js". If such a file already exists, the it is overwritten. This prevents to get files like "<old name>.min.min.min.js" if you run `minify-all-js` multiple times.
 
 ### Installation
 
@@ -9,18 +11,22 @@ A function that minifies your javascript files (recursively).
 
 ### Run CLI
 
-    > minify-all-js [folder] [-j] [-m] [-M] [-p]
+    > minify-all-js [inputFolder] [outputFolder] [-j] [-m] [-M] [-p] [-o]
 
 ##### Sample
 
     > minify-all-js ./node_modules -j -m -M -p
 
 Use CLI options:
+ - `inputFolder` folder of input files or current directory if undefined
+ - `outputFolder` folder for output files or `inputFolder` if undefined
  - `-j` or `--json` to compress json files as well. (default: `false`)
  - `-m` or `--module` to set terser module option to `true` for ES6 files (default: `false`)
  - `-M` or `--mangle` to set terser mangle option to `true` (default: `false`)
- - `-p` or `--packagejson` to clean up extra fields from package.json files (default: false)
- - `-a` or `--all` to try to compress all files including binary/executable js files without `.js` extension (default: false)
+ - `-p` or `--packagejson` to clean up extra fields from package.json and files (default: `false`)
+ - `-a` or `--all` to try to compress all files including binary/executable js files without `.js` extension (default: `false`)
+ - `-o` or `--overwrite` to overwrite original files. If `true` then `outputFolder` is ignored (default: `false`)
+
 
 ### Run programatically
 
--- index.js
+++ index.js
@@ -14,79 +14,81 @@ var failed_files = []
 var total_files = 0
 var options = {}
 
-const minifyJS = async file => {
-  total_files ++
+const minifyJS = async (inFile, outFile) => {
+  total_files++
+  //console.log(`${inFile} -> ${outFile}`);
   try {
     await minify({
       compressor: terser,
-      input: file,
-      output: file,
+      input: inFile,
+      output: outFile,
       options: {
         module: options.module,
         mangle: options.mangle,
         compress: { reduce_vars: false }
       }
     })
-  } catch(e) {
-    failed_files.push(file)
+  } catch (e) {
+    failed_files.push(inFile)
   }
   process.stdout.write('.')
 }
 
-const minifyJSON = async file => {
+const minifyJSON = async (inFile, outFile) => {
   try {
     if ((options.compress_json || options.packagejson)) {
-      total_files ++
-      var is_package_json = file.indexOf('package.json') > -1
-      var data = await readFile(file, 'utf8')
+      total_files++
+      var is_package_json = inFile.endsWith('package.json');
+      var data = await readFile(inFile, 'utf8')
       var json = JSON.parse(data)
       var new_json = {}
       if (options.packagejson && is_package_json) {
         var {
           name, version, bin, main, binary, engines
         } = json
-        new_json = {name, version}
+        new_json = { name, version }
         if (bin) new_json.bin = bin
         if (binary) new_json.binary = binary
         if (main) new_json.main = main
         if (engines) new_json.engines = engines
-      } else {
-        new_json = json
+        await writeFile(outFile, JSON.stringify(new_json))
       }
-      await writeFile(file, JSON.stringify(new_json))
     }
-  } catch(e) {}
+  } catch (e) { }
   process.stdout.write('.')
 }
 
-const walk = async (currentDirPath) => {
+const walk = async (inputDir, outputDir, overwrite) => {
   var js_files = []
   var json_files = []
   var dirs = []
-  var current_dirs = await readdir(currentDirPath)
+  var current_dirs = await readdir(inputDir)
   current_dirs.forEach(name => {
-    var filePath = path.join(currentDirPath, name);
+    var filePath = path.join(inputDir, name);
     var stat = fs.statSync(filePath);
-    var is_bin = /\.bin$/
+    var outFile = path.join(outputDir, overwrite ? name : `${path.basename(name, path.extname(name))}.min${path.extname(name)}`);
+    if (!overwrite && path.basename(name, path.extname(name)).endsWith(".min"))
+      outFile = path.join(outputDir, name);
     if (stat.isFile()) {
-      if (filePath.substr(-5) === ".json")
-        json_files.push(filePath)
-      else if (filePath.substr(-3) === ".js" || options.all_js)
-        js_files.push(filePath)
-    } else if (stat.isDirectory() && !is_bin.test(filePath)) {
+      if (filePath.endsWith(".json"))
+        json_files.push({ in: filePath, out: outFile })
+      else if (filePath.endsWith(".js") || options.all_js)
+        js_files.push({ in: filePath, out: outFile })
+    } else if (overwrite && stat.isDirectory() && !filePath.endsWith(".bin")) {
       dirs.push(filePath)
     }
   })
-  var js_promise = Promise.all(js_files.map(f => minifyJS(f)))
-  var json_promise = Promise.all(json_files.map(f => minifyJSON(f)))
+  var js_promise = Promise.all(js_files.map(f => minifyJS(f.in, f.out)))
+  var json_promise = Promise.all(json_files.map(f => minifyJSON(f.in, f.out)))
   await Promise.all([js_promise, json_promise])
-  await promiseSeries(dirs.map(dir => () => walk(dir)))
+  if (overwrite)
+    await promiseSeries(dirs.map(dir => () => walk(dir, dir, overwrite)))
 }
 
-async function minifyAll (dir, opts){
+async function minifyAll(inputDir, outputDir, opts, overwrite) {
   Object.assign(options, opts || {})
   console.log('minify-all-js options:\n', JSON.stringify(options, null, 2))
-  await walk(dir);
+  await walk(inputDir, outputDir, overwrite);
   process.stdout.write('.\n')
   console.log('Total found files: ' + total_files)
   if (failed_files.length) {
@@ -98,15 +100,25 @@ async function minifyAll (dir, opts){
 if (require.main === module) {
   var input = process.argv;
   var inputDir = input[2] || process.cwd();
-  var opts = {}
+  if (inputDir.startsWith("-"))
+    inputDir = process.cwd();
 
+  var outputDir = input[3] || inputDir;
+  if (outputDir.startsWith("-"))
+    outputDir = inputDir;
+
+  var opts = {}
   opts.compress_json = input.includes('--json') || input.includes('-j') || false
   opts.module = input.includes('--module') || input.includes('-m') || false
   opts.mangle = input.includes('--mangle') || input.includes('-M') || false
   opts.packagejson = input.includes('--packagejson') || input.includes('-p') || false
   opts.all_js = input.includes('--all') || input.includes('-a') || false
 
-  minifyAll(inputDir, opts);
+  var overwrite = input.includes('--overwrite') || input.includes('-o') || false;
+  if (overwrite)
+    outputDir = inputDir;
+
+  minifyAll(inputDir, outputDir, opts, overwrite);
 
 } else {
   module.exports = minifyAll;
